cb1d24520173cb0369b7f16acd7c0f8ce2616470
GibboK <gibbok.coding@gmail.com>
2017-06-10 12:28:47 +0200

    Add examples

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+﻿var gulp = require('gulp');
+var connect = require('gulp-connect');
+var open = require('open');
+var jscs = require('gulp-jscs');
+var jshint = require('gulp-jshint');
+var stylish = require('gulp-jscs-stylish');
+
+gulp.task('connect', function () {
+    // runs connect server
+    connect.server({
+        root: ""
+    });
+});
+
+gulp.task('open', function () {
+    // open browser
+    var uri = 'http://localhost:8080/examples/standard.html';
+    open(uri);
+});
+
+gulp.task('checkcode', function () {
+    // validate source code using jscs and jshint
+    gulp.src('.')
+        .pipe(jshint())
+        .pipe(jscs())
+        .on('error', function () { })
+        .pipe(stylish.combineWithHintResults())
+        .pipe(jshint.reporter('jshint-stylish'));
+});

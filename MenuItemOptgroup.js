5ad587d7aceaa899db9cabff69836cbe0fcac43f
GibboK <gibbok@users.noreply.github.com>
2017-06-15 15:56:51 +0200

    Add cleanup

--- MenuItemOptgroup.js
+++ MenuItemOptgroup.js
@@ -2,10 +2,10 @@
 
 define([
     'dojo/_base/declare',
-    "dojo/dom-attr",
-    "dojo/dom-class",
-    "dojo/dom",
-    'dijit/MenuItem',
+    'dojo/dom-attr',
+    'dojo/dom-class',
+    'dojo/dom',
+    'dijit/MenuItem'
 ], function (
     declare,
     domAttr,
@@ -19,14 +19,14 @@ define([
                 //      Customize the template for rendering.
                 //      Add `optgroup` and `option` CSS classes to `domNode`.
                 this.inherited(arguments);
-                var label = this.id + "_text";
-                var cssClass = this.option.optgroup === true ? 'SelectOptgroup__flattened' : 'SelectOptgroup__nested';
-                domAttr.set(this.containerNode, "id", label); // only needed for backward compat
+                var label = this.id + '_text',
+                    cssClass = this.option.optgroup === true ? 'SelectOptgroup__flattened' : 'SelectOptgroup__nested';
+                domAttr.set(this.containerNode, 'id', label); // only needed for backward compat
                 if (this.accelKeyNode) {
-                    domAttr.set(this.accelKeyNode, "id", this.id + "_accel"); // only needed for backward compat
+                    domAttr.set(this.accelKeyNode, 'id', this.id + '_accel'); // only needed for backward compat
                 }
                 dom.setSelectable(this.domNode, false);
                 domClass.add(this.domNode, cssClass);
-            },
+            }
         });
     });

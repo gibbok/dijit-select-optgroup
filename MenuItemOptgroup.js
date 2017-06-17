57d913f27dceb868ae432cbe2f8271d6d4af9252
GibboK <gibbok@users.noreply.github.com>
2017-06-17 08:42:02 +0200

    Add code cleanup

--- MenuItemOptgroup.js
+++ MenuItemOptgroup.js
@@ -1,9 +1,10 @@
-﻿define([
+/*! dijit-select-optgroup | The MIT License (MIT) | Copyright (c) 2017 GibboK */
+define([
     'dojo/_base/declare',
-    "dojo/dom-attr",
-    "dojo/dom-class",
-    "dojo/dom",
-    'dijit/MenuItem',
+    'dojo/dom-attr',
+    'dojo/dom-class',
+    'dojo/dom',
+    'dijit/MenuItem'
 ], function (
     declare,
     domAttr,
@@ -17,14 +18,14 @@
                 //      Customize the template for rendering.
                 //      Add `optgroup` and `option` CSS classes to `domNode`.
                 this.inherited(arguments);
-                var label = this.id + "_text";
-                var cssClass = this.option.optgroup === true ? 'SelectOptgroup__flattened' : 'SelectOptgroup__nested';
-                domAttr.set(this.containerNode, "id", label); // only needed for backward compat
+                var label = this.id + '_text',
+                    cssClass = this.option.optgroup === true ? 'SelectOptgroup__flattened' : 'SelectOptgroup__nested';
+                domAttr.set(this.containerNode, 'id', label); // only needed for backward compat
                 if (this.accelKeyNode) {
-                    domAttr.set(this.accelKeyNode, "id", this.id + "_accel"); // only needed for backward compat
+                    domAttr.set(this.accelKeyNode, 'id', this.id + '_accel'); // only needed for backward compat
                 }
                 dom.setSelectable(this.domNode, false);
                 domClass.add(this.domNode, cssClass);
-            },
+            }
         });
     });
